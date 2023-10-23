df3da2841b4766bc113053c5e5062dc21fd7e743
Yaroslav82 <yargay230607@gmail.com>
2023-10-23 14:25:27 +0300

    Create Constants

---
+++ src/main/java/com/example/app/SpringBootWebNaturix/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.example.app.SpringBootWebNaturix.utils;
+
+public final class Constants {
+
+    public final static String BASE_URL = "http://localhost:8080/";
+    public final static String URL_IMAGES_UPLOADS = "images/uploads/";
+    // Коли використовував ./images/uploads/ не працювало
+    public final static String URL_FILE_UPLOADS = "src/main/resources/static/images/uploads/";
+}
