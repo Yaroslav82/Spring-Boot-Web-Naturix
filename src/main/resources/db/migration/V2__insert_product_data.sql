719f5e903267dfcdbc51abdd62b671556d3c0eb8
Yaroslav82 <yargay230607@gmail.com>
2024-08-11 13:27:05 +0300

    Move SQLs.sql to flyway migrations

---
+++ src/main/resources/db/migration/V2__insert_product_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+ products (img, name, type, price)
+VALUES
+ ('1.png', 'tomato', 'vegetable', 9.99),
+ ('2.png', 'cabbage', 'vegetable', 5.99),
+ ('3.png', 'cherry', 'fruit', 19.99),
+ ('4.png', 'celery', 'vegetable', 3.99),
+ ('5.png', 'pineapple', 'fruit', 29.99),
+ ('6.png', 'onion', 'vegetable', 4.99),
+ ('7.png', 'mushroom', 'vegetable', 34.99),
+ ('8.png', 'pepper', 'vegetable', 14.99);
